51ff0406b10ab4a9f932ed57a1cdce006a1d194f
super <you@example.com>
2016-12-12 18:50:06 +0800

    add server

--- server.js
+++ server.js
@@ -6,14 +6,68 @@ var path = require('path');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function(req,res){
 
-    console.log('111')
-    res.send('shshsh');
-    res.end();
+ http.createServer(function(req,res){
+
+    var query = url.parse(req.url,true);
+     const path = query.pathname;
+     console.log(path);
+     switch (path){
+         case '/getStudent':{
+             fs.readFile(__dirname+'/1.json','utf8',function(err,data){
+                 console.log(data);
+                 res.writeHead(200,{"Content-Type":"text/json","Access-Control-Allow-Origin":"*"});
+                 res.end(data,'utf8')
+             });
+             break;
+
+
+         }
+         case '/putStudent':{
+             fs.readFile(__dirname+'/1.json','utf8',function(err,data){
+                 console.log(req.body);
+                data = JSON.parse(data);
+                 var str = ""
+                 req.on('data',function(chunk){
+                     str +=chunk;
+                 })
+                 req.on('end',function(){
+                        console.log((str))
+                     data.push(JSON.parse(str));
+                     fs.writeFile(__dirname+'/1.json',JSON.stringify(data));
+                     res.end();
+                 })
+             });
+             break;
+         }
+         case '/deleteStudent':{
+             fs.readFile(__dirname+'/1.json','utf8',function(err,data){
+                 console.log(req.body);
+                 data = JSON.parse(data);
+                 var str = ""
+                 req.on('data',function(chunk){
+                     str +=chunk;
+                 })
+                 req.on('end',function(){
+                     console.log((str))
+                     data.forEach(function(item,index){
+                         if(JSON.stringify(item) == str){
+                             data.splice(index,1);
+                             fs.writeFile(__dirname+'/1.json',JSON.stringify(data));
+                             res.end();
+                             return ;
+                         }
+
+                     });
+
+                 })
+             });
+             break;
+
+         }
+     }
 
 
 
-}).listen(function(){
-    console.log('9000')
-},9090)
+}).listen(9000) ;
+
