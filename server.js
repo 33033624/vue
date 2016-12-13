ff5933217ca4923285b4d21fb18679eebea84305
super <you@example.com>
2016-12-13 13:07:02 +0800

    change

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ var fs = require('fs');
                         console.log((str))
                      data.push(JSON.parse(str));
                      fs.writeFile(__dirname+'/1.json',JSON.stringify(data));
+                     res.writeHead(200,{"Content-Type":"text/json","Access-Control-Allow-Origin":"*"});
                      res.end();
                  })
              });
@@ -54,6 +55,39 @@ var fs = require('fs');
                          if(JSON.stringify(item) == str){
                              data.splice(index,1);
                              fs.writeFile(__dirname+'/1.json',JSON.stringify(data));
+                             res.writeHead(200,{"Content-Type":"text/json","Access-Control-Allow-Origin":"*"});
+                             res.end();
+                             return ;
+                         }
+
+                     });
+
+                 })
+             });
+             break;
+
+         }
+         case '/changeStudent':{
+             fs.readFile(__dirname+'/1.json','utf8',function(err,data){
+                 console.log(req.body);
+                 data = JSON.parse(data);
+                 var str = ""
+                 req.on('data',function(chunk){
+                     str +=chunk;
+                 })
+                 req.on('end',function(){
+                     console.log((str))
+                     console.log('changeStudnet====>');
+                     str = JSON.parse(str);
+                     data.forEach(function(item,index){
+
+
+                         console.log('item');
+                         if(item.name ==str.name ){
+                             console.log('1111')
+                             data.splice(index,1,str);
+                             fs.writeFile(__dirname+'/1.json',JSON.stringify(data));
+                             res.writeHead(200,{"Content-Type":"text/json","Access-Control-Allow-Origin":"*"});
                              res.end();
                              return ;
                          }
